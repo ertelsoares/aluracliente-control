36bf519bfedc1e1685e24a129715b27aba380d3e
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2024-04-24 21:56:59 -0300

    Salvando aprendizado do provider
    Aprendizado do provider para controle de estado.

---
+++ lib/models/clients.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:client_control/models/client.dart';
+class Clients extends ChangeNotifier{
+   List<Client> clients;
+
+   Clients({
+       required this.clients
+    });
+}
